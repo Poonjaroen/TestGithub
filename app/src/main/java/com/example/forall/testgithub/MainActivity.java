fa654796f4a96e761156ddbc73ada425c39d0344
Wasit Poonjaroen <wasitfang@gmail.com>
2015-10-29 22:08:03 +0700

    Initial first commit

---
+++ app/src/main/java/com/example/forall/testgithub/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.forall.testgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    String name = "name";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
