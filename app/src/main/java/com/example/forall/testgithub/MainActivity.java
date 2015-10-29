97f1c87043ab46992fbdab30b407dcf653d64ba4
Wasit Poonjaroen <wasitfang@gmail.com>
2015-10-29 22:11:13 +0700

    update code int id

--- app/src/main/java/com/example/forall/testgithub/MainActivity.java
+++ app/src/main/java/com/example/forall/testgithub/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     String name = "name";
-
+    int id = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
